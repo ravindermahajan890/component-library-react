6861667a0fc48ed7756f65857d8fc91d0d6eb58c
Ravinder Mahajan <88770719+ravinder-tw@users.noreply.github.com>
2021-10-01 16:52:15 +0530

    setup(library): updated version

--- src/Components/Button/Index.tsx
+++ src/Components/Button/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ name }: { name: string }): JSX.Element => (
-  <div>Hey {name}, say hello to TypeScript.</div>
+  <div>Hey {name}, say hello to Button.</div>
 );
 
 export default Button;
